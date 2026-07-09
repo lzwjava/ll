69c598c6f4c7e18a0a799516a607696489cced88
Zhiwei Li <lzwjava@gmail.com>
2026-07-09 18:59:31 +0800

    feat: blink LED every 3 seconds

--- sketch_jul9a/sketch_jul9a.ino
+++ sketch_jul9a/sketch_jul9a.ino
@@ -1,9 +1,11 @@
 void setup() {
-  // put your setup code here, to run once:
-
+  // initialize digital pin LED_BUILTIN as an output.
+  pinMode(LED_BUILTIN, OUTPUT);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  digitalWrite(LED_BUILTIN, HIGH);  // turn the LED on (HIGH is the voltage level)
+  delay(3000);                      // wait for 3 seconds
+  digitalWrite(LED_BUILTIN, LOW);   // turn the LED off
+  delay(3000);                      // wait for 3 seconds
 }
